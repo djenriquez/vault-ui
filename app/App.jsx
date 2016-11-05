0fce699f5fd1dba95dc72ae9bed35d64cf993f35
Robert Lippens <rlippens39@gmail.com>
2016-11-04 17:55:22 -0700

    Adding fancy login animation and borrowing vault's image for favicon/logo

--- app/App.jsx
+++ app/App.jsx
@@ -8,13 +8,21 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 injectTapEventPlugin();
 
+const checkAuthToken = (nextState, replace, callback) => {
+    let vaultAuthToken = window.localStorage.getItem('vaultAuthenticationToken');
+    if (!vaultAuthToken) {
+        replace(`/login`)
+    }
+    callback();
+}
+
 ReactDOM.render((
     <MuiThemeProvider>
   <Router history={browserHistory}>
-    <Route path="/" component={Login}>
       <Route path="/login" component={Login}/>
-      <Route path="*" component={Login}/>
-    </Route>
+      <Route path="/" component={Home} onEnter={checkAuthToken}>
+          <Route path="*" component={Login}/>
+      </Route>
   </Router>
   </MuiThemeProvider>
 ), document.getElementById('app'))
--- app/components/Login/Login.jsx
+++ app/components/Login/Login.jsx
@@ -1,17 +1,42 @@
 import React, { PropTypes } from 'react'
 import styles from './login.css';
 import TextField from 'material-ui/TextField';
+import { browserHistory } from 'react-router'
 
 export default class Login extends React.Component {
+    constructor(props) {
+      super(props);
+      this.validateAuthToken = this.validateAuthToken.bind(this);
+      this.state = {
+          show: false,
+          authToken: ""
+      };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ show: true}, 2000);
+        });
+    }
+
+    validateAuthToken(e) {
+        if (e.keyCode === 13) {
+            window.localStorage.setItem("vaultAuthenticationToken",this.state.authToken);
+            window.location.href = '/';
+        }
+    }
+
     render () {
         return (
             <div id={styles.root} className="row middle-xs center-xs">
-                <div className="col-xs-12 col-sm-6 col-md-4">
-                    <div className="col-xs-12" id={styles.title}>VAULT</div>
+                <div className={`col-xs-12 col-sm-6 col-md-4 ${this.state.show ? styles.show : styles.hide}`}>
+                    <div className="col-xs-12" id={styles.title}><img height="40" src="https://www.vaultproject.io/assets/images/favicon-16466d1a.png"></img>AULT</div>
                     <TextField
                         fullWidth={true}
                         className="col-xs-12"
                         hintText="Enter authentication token"
+                        onKeyDown={this.validateAuthToken}
+                        onChange={(e,v)=>this.setState({authToken: v})}
                     />
                 </div>
             </div>
