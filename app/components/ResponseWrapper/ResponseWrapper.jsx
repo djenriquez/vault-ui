9fdaf43da5030f7a32e95cc4cd0df413c8e0bebd
DJ Enriquez <denrie.enriquezjr@gmail.com>
2016-11-22 23:42:31 -0800

    Adding response wrapping functionality
    - Wrap Value takes a TTL + Value
    - Wrap Secret takes a TTL + Secret Key
    - Unwrap takes a token value

--- app/components/ResponseWrapper/ResponseWrapper.jsx
+++ app/components/ResponseWrapper/ResponseWrapper.jsx
@@ -4,6 +4,8 @@ import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+import { green500, green400, red500, red300, yellow500, white } from 'material-ui/styles/colors.js'
 
 import _ from 'lodash';
 
@@ -17,18 +19,81 @@ export default class ResponseWrapper extends React.Component {
             WrapType: '',
             WrapValue: '',
             WrappedToken: '',
-            WrapTTL: ''
+            WrappedSecretKey: '',
+            WrapTTL: '',
+            submitBtnColor: 'lightgrey',
+            submitBtnDisabled: true
         };
 
-        _.bindAll(this,
-            'checkTTLValue');
+        _.bindAll(
+            this,
+            'checkTTLValue',
+            'submitBtnClick',
+            'checkValue',
+            'checkWrappedToken',
+            'checkSecretKey'
+        );
+    }
+
+    restoreStateDefaults() {
+        this.setState({
+            WrapValue: '',
+            WrappedToken: '',
+            WrappedSecretKey: '',
+            WrapTTL: '',
+            submitBtnColor: 'lightgrey',
+            submitBtnDisabled: true
+        });
     }
 
     checkTTLValue(e, v) {
         //Try to parse as an int, if failed, return error
+        if (!isNaN(v) && v.indexOf('.') === -1) {
+            let buttonColor = (v && (this.state.WrapValue || this.state.WrappedSecretKey) && v > 0) ? green500 : 'lightgrey';
+            this.setState({
+                WrapTTL: v,
+                submitBtnColor: buttonColor,
+                submitBtnDisabled: !(v && (this.state.WrapValue || this.state.WrappedSecretKey))
+            });
+        }
+    }
+
+    checkValue(e, v) {
+        let buttonColor = (v && !isNaN(this.state.WrapTTL) && this.state.WrapTTL > 0) ? green500 : 'lightgrey';
         this.setState({
-            WrapTTL: v
+            WrapValue: v,
+            submitBtnColor: buttonColor,
+            submitBtnDisabled: !(v && this.state.WrapValue)
         });
+    }
+
+    checkWrappedToken(e, v) {
+        let buttonColor = v ? green500 : 'lightgrey';
+        this.setState({
+            WrappedToken: v,
+            submitBtnColor: buttonColor,
+            submitBtnDisabled: !v
+        });
+    }
+
+    checkSecretKey(e, v) {
+        let buttonColor = v ? green500 : 'lightgrey';
+        this.setState({
+            WrappedSecretKey: v,
+            submitBtnColor: buttonColor,
+            submitBtnDisabled: !v
+        });
+    }
+
+    submitBtnClick() {
+        switch (this.state.WrapType) {
+            case "WRAPVALUE":
+                break;
+            case "WRAPSECRET":
+                break;
+            case "UNWRAP":
+                break;
+        }
     }
 
     renderWrapFunction() {
@@ -36,38 +101,53 @@ export default class ResponseWrapper extends React.Component {
             case "WRAPVALUE":
                 return (
                     <div>
-                        <h3>Wrap Value</h3>
-                        <div className="row">
+                        <div>
                             <TextField
                                 id="text-field-wrap-TTL"
                                 value={this.state.WrapTTL}
                                 onChange={this.checkTTLValue}
                                 floatingLabelText="TTL in seconds"
                                 />
+                        </div>
+                        <div>
                             <TextField
                                 id="text-field-wrap"
                                 value={this.state.WrapValue}
-                                onChange={(e, v) => { this.setState({ WrapValue: v }) } }
+                                onChange={this.checkValue}
                                 multiLine={true}
                                 floatingLabelText="Value to wrap"
                                 />
                         </div>
+
                     </div>
                 )
             case "WRAPSECRET":
                 return (
                     <div>
-                        <h3>Wrap Secret</h3>
+                        <div>
+                            <TextField
+                                id="text-field-wrap-TTL"
+                                value={this.state.WrapTTL}
+                                onChange={this.checkTTLValue}
+                                floatingLabelText="TTL in seconds"
+                                />
+                        </div>
+                        <TextField
+                            id="text-field-wrap"
+                            value={this.state.WrappedSecretKey}
+                            onChange={this.checkSecretKey}
+                            floatingLabelText="Secret Key to wrap"
+                            />
                     </div>
                 )
             case "UNWRAP":
                 return (
                     <div>
-                        <h3>Wrapped Response Token</h3>
                         <TextField
                             id="text-field-unwrap"
-                            value={this.state.WrapValue}
-                            onChange={(e, v) => { this.setState({ WrappedToken: v }) } }
+                            floatingLabelText="Token to unwrap"
+                            value={this.state.WrappedToken}
+                            onChange={this.checkWrappedToken}
                             />
                     </div>
                 )
@@ -80,7 +160,10 @@ export default class ResponseWrapper extends React.Component {
 
     render() {
         let handleSelectFieldChange = (e, i, v) => {
-            this.setState({ WrapType: v });
+            this.restoreStateDefaults();
+            this.setState({
+                WrapType: v,
+            });
         }
 
         return (
@@ -98,6 +181,15 @@ export default class ResponseWrapper extends React.Component {
                     </SelectField>
                 </div>
                 {this.renderWrapFunction()}
+                <div>
+                    <FlatButton
+                        label="Submit"
+                        backgroundColor={this.state.submitBtnColor}
+                        hoverColor={green400}
+                        disabled={this.state.submitBtnDisabled}
+                        labelStyle={{ color: white }}
+                        onTouchTap={() => this.submitBtnClick()} />
+                </div>
             </div>
         )
     }
