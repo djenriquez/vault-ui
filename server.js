58b36c0f9ff9c77c63cbc281d312b27765e2afff
Amanda Edades <amanda.edades@infospace.com>
2016-12-08 20:54:52 -0800

    Listen for /listsecretbackends

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.post('/login', function (req, res) {
     routeHandler.login(req, res);
 });
 
+app.get('/listsecretbackends', function (req, res) {
+    routeHandler.listSecretBackends(req, res);
+});
+
 app.get('/listsecrets', function (req, res) {
     routeHandler.listSecrets(req, res);
 });
