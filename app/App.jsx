5b45be9162ec28b7791cedcb3b35b828b8e513b8
djenriquez <denrie.enriquezjr@gmail.com>
2017-03-25 01:36:08 -0700

    Adding Okta skeleton

--- app/App.jsx
+++ app/App.jsx
@@ -14,6 +14,7 @@ import TokenAuthBackend from './components/Authentication/Token/Token.jsx';
 import AwsEc2AuthBackend from './components/Authentication/AwsEc2/AwsEc2.jsx';
 import GithubAuthBackend from './components/Authentication/Github/Github.jsx';
 import RadiusAuthBackend from './components/Authentication/Radius/Radius.jsx';
+import OktaAuthBackend from './components/Authentication/Okta/Okta.jsx';
 import SecretUnwrapper from './components/shared/Wrapping/Unwrapper';
 
 injectTapEventPlugin();
@@ -58,6 +59,7 @@ ReactDOM.render((
                 <Route path="/auth/aws-ec2/:namespace" component={AwsEc2AuthBackend} />
                 <Route path="/auth/github/:namespace" component={GithubAuthBackend} />
                 <Route path="/auth/radius/:namespace(/**)" component={RadiusAuthBackend} />
+                <Route path="/auth/okta/:namespace(/**)" component={OktaAuthBackend} />
                 <Route path="/settings" component={Settings} />
                 <Route path="/responsewrapper" component={ResponseWrapper} />
                 <Route path="/sys/policies(/**)" component={PolicyManager} />
--- app/components/shared/Menu/Menu.jsx
+++ app/components/shared/Menu/Menu.jsx
@@ -20,7 +20,8 @@ const supported_secret_backend_types = [
 const supported_auth_backend_types = [
     'token',
     'github',
-    'radius'
+    'radius',
+    'okta'
 ]
 
 function snackBarMessage(message) {
