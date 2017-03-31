546e3589137e5b9ba3f5c03715edbe8be70a8589
Matteo Sessa <matteo.sessa@catchoftheday.com.au>
2017-03-31 22:33:37 +1100

    Remove unused code

--- server.js
+++ server.js
@@ -3,19 +3,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var axios = require('axios');
-var _ = require('lodash');
 var routeHandler = require('./src/routeHandler');
 var compression = require('compression');
 
 var PORT = 8000;
-var VAULT_URL_DEFAULT = process.env.VAULT_URL_DEFAULT || "";
-var VAULT_URL_DEFAULT_FORCE = process.env.VAULT_URL_DEFAULT_FORCE ? true : false;
-var VAULT_AUTH_DEFAULT = process.env.VAULT_AUTH_DEFAULT || "GITHUB";
-var VAULT_AUTH_DEFAULT_FORCE = process.env.VAULT_AUTH_DEFAULT_FORCE ? true : false;
-var VAULT_AUTH_BACKEND_PATH = process.env.VAULT_AUTH_BACKEND_PATH
-var VAULT_AUTH_BACKEND_PATH_FORCE = process.env.VAULT_AUTH_BACKEND_PATH_FORCE ? true : false;
-var VAULT_SUPPLIED_TOKEN_HEADER = process.env.VAULT_SUPPLIED_TOKEN_HEADER
 
 var app = express();
 app.set('view engine', 'html');
@@ -42,17 +33,12 @@ app.get('/vaultui', function(req,res) {
     routeHandler.vaultuiHello(req, res);
 });
 
-app.all('/v1/*', function(req, res, next) {
+app.all('/v1/*', function(req, res) {
     routeHandler.vaultapi(req, res);
 })
 
 app.get('/');
 
 app.get('*', function (req, res) {
-    res.render(path.join(__dirname, '/index.html'),{
-        defaultUrl: VAULT_URL_DEFAULT,
-        defaultAuth: VAULT_AUTH_DEFAULT,
-        suppliedAuthToken: VAULT_SUPPLIED_TOKEN_HEADER ? req.header(VAULT_SUPPLIED_TOKEN_HEADER) : "",
-        defaultBackendPath: VAULT_AUTH_BACKEND_PATH
-    });
+    res.render(path.join(__dirname, '/index.html'));
 });
