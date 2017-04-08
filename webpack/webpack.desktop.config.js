29de78972c44a9cdb6de3b2cdf096fc05efb9411
Matteo Sessa <matteo.sessa@catchoftheday.com.au>
2017-04-08 20:33:12 +1000

    Bundle desktop under different name

--- webpack/webpack.desktop.config.js
+++ webpack/webpack.desktop.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var config = require('./webpack.min.config');
 
 config.target = "electron";
+config.output.filename = "desktopbundle.js";
 config.plugins.push(new webpack.DefinePlugin({WEBPACK_DEF_TARGET_WEB: false}));
 
 module.exports = config;
