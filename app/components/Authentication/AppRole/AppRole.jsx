748ba61e257d6fee1313bc08dc313255a99f3d68
djenriquez <denrie.enriquezjr@gmail.com>
2017-08-18 23:36:07 -0700

    Add property fields + fix create/update

--- app/components/Authentication/AppRole/AppRole.jsx
+++ app/components/Authentication/AppRole/AppRole.jsx
@@ -32,11 +32,11 @@ function snackBarMessage(message) {
 
 export default class AppRoleAuthBackend extends React.Component {
     static propTypes = {
-        params: PropTypes.object.isRequired
+        params: PropTypes.object.isRequired,
+        location: PropTypes.object.isRequired
     };
 
     itemConfigSchema = {
-        role_name: undefined,
         bind_secret_id: undefined,
         bind_cidr_list: undefined,
         policies: [],
@@ -67,13 +67,47 @@ export default class AppRoleAuthBackend extends React.Component {
 
         _.bindAll(
             this,
-            'listAppRoles'
+            'listAppRoles',
+            'getAppRoleConfig'
         );
     }
 
     // Events
     componentDidMount() {
         this.listAppRoles();
+        // If an approle is requested by URI
+        let uri = this.props.location.pathname.split(this.state.baseUrl)[1];
+        let appRole = uri.split('/')[1];
+        if (appRole) {
+            this.setState({ selectedItemName: appRole });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.selectedItemName && (this.state.selectedItemName !== prevState.selectedItemName)) {
+            this.getAppRoleConfig();
+        }
+    }
+
+    getAppRoleConfig() {
+        tokenHasCapabilities(['read'], `${this.state.baseVaultPath}/role/${this.state.selectedItemName}`)
+            .then(() => {
+                callVaultApi('get', `${this.state.baseVaultPath}/role/${this.state.selectedItemName}`, null, null)
+                    .then((resp) => {
+                        let appRole = _.get(resp, 'data.data', {});
+                        this.setState({ itemConfig: appRole, openEditItemDialog: true });
+                    })
+                    .catch((error) => {
+                        if (error.response.status !== 404) {
+                            snackBarMessage(error);
+                        } else {
+                            this.setState({ itemConfig: this.itemConfigSchema });
+                        }
+                    })
+            })
+            .catch(() => {
+                snackBarMessage(new Error('Access denied'));
+            })
     }
 
     listAppRoles() {
@@ -105,46 +139,30 @@ export default class AppRoleAuthBackend extends React.Component {
 
                     })
                     .catch((error) => {
-
+                        snackBarMessage(error);
                     })
             })
             .catch(() => {
                 snackBarMessage(new Error('Access denied'));
             })
-    }
-
-    getAppRoleConfig() {
-        tokenHasCapabilities(['read'], `${this.state.baseVaultPath}/role/${this.state.selectedItemName}`)
-            .then(() => {
-                callVaultApi('get', `${this.state.baseVaultPath}/role/${this.state.selectedItemName}`, { list: true }, null)
-                    .then((resp) => {
-
-                    })
-                    .catch((error) => {
-
-                    })
-            })
-            .catch(() => {
-                snackBarMessage(new Error('Access denied'));
-            })
-
     }
 
     createUpdateItem() {
-        tokenHasCapabilities(['create', 'update'], `${this.state.baseVaultPath}/role/${this.state.itemConfig.role_name}`)
+        tokenHasCapabilities(['create', 'update'], `${this.state.baseVaultPath}/role/${this.state.selectedItemName}`)
             .then(() => {
                 let updateObj = _.clone(this.state.itemConfig);
                 if (updateObj.policies.length > 0)
                     updateObj.policies = this.state.itemConfig.policies.join(',');
-                callVaultApi('post', `${this.state.baseVaultPath}/role/${this.state.itemConfig.role_name}`, null, updateObj)
+                callVaultApi('post', `${this.state.baseVaultPath}/role/${this.state.selectedItemName}`, null, updateObj)
                     .then((resp) => {
-
+                        history.push(`${this.state.baseUrl}`);
+                        this.setState({ openEditItemDialog: false, openNewItemDialog: false, itemConfig: this.itemConfigSchema, selectedItemName: '' })
                     })
                     .catch((error) => {
-
+                        snackBarMessage(error);
                     })
             })
-            .catch((error) => {
+            .catch(() => {
                 snackBarMessage(new Error('Access denied'));
             })
     }
@@ -163,7 +181,7 @@ export default class AppRoleAuthBackend extends React.Component {
                             fullWidth={false}
                             autoFocus
                             onChange={(e) => {
-                                this.setState({ itemConfig: update(this.state.itemConfig, { role_name: { $set: e.target.value } }) });
+                                this.setState({ selectedItemName: e.target.value });
                             }}
                         />
                     ]
@@ -171,7 +189,24 @@ export default class AppRoleAuthBackend extends React.Component {
             }
 
             let renderEditFields = () => {
-
+                return (
+                    [
+                        <TextField
+                            key='role_id'
+                            className={styles.textFieldStyle}
+                            hintText={`Enter the Role ID`}
+                            value={this.state.itemConfig.role_id}
+                            floatingLabelFixed={true}
+                            floatingLabelText='Role ID'
+                            fullWidth={false}
+                            autoFocus
+                            onChange={(e) => {
+                                let val = e.target.value.split(',');
+                                this.setState({ itemConfig: update(this.state.itemConfig, { role_id: { $set: val } }) });
+                            }}
+                        />
+                    ]
+                )
             }
 
             let renderConstantFields = () => {
@@ -180,12 +215,15 @@ export default class AppRoleAuthBackend extends React.Component {
                         <ListItem primaryText='Bind Secret ID'>
                             <Toggle
                                 toggled={this.state.itemConfig.bind_secret_id}
+                                value={this.state.bind_secret_id}
                                 onToggle={(e, v) => {
                                     this.setState({ itemConfig: update(this.state.itemConfig, { bind_secret_id: { $set: v } }) });
                                 }}
                             />
                         </ListItem>,
                         <TextField
+                            key='bind_cidr_list'
+                            value={this.state.itemConfig.bind_cidr_list}
                             className={styles.textFieldStyle}
                             hintText={`Enter the CIDR blocks`}
                             floatingLabelFixed={true}
@@ -198,6 +236,8 @@ export default class AppRoleAuthBackend extends React.Component {
                             }}
                         />,
                         <TextField
+                            key='secret_id_num_uses'
+                            value={this.state.itemConfig.secret_id_num_uses}
                             className={styles.textFieldStyle}
                             hintText={`# of Secret ID uses`}
                             floatingLabelFixed={true}
@@ -207,6 +247,71 @@ export default class AppRoleAuthBackend extends React.Component {
                             onChange={(e) => {
                                 this.setState({ itemConfig: update(this.state.itemConfig, { secret_id_num_uses: { $set: e.target.value } }) });
                             }}
+                        />,
+                        <TextField
+                            key='secret_id_ttl'
+                            value={this.state.itemConfig.secret_id_ttl}
+                            className={styles.textFieldStyle}
+                            hintText={`Secret ID TTL`}
+                            floatingLabelFixed={true}
+                            floatingLabelText='Secret ID TTL'
+                            fullWidth={false}
+                            autoFocus
+                            onChange={(e) => {
+                                this.setState({ itemConfig: update(this.state.itemConfig, { secret_id_ttl: { $set: e.target.value } }) });
+                            }}
+                        />,
+                        <TextField
+                            key='token_num_uses'
+                            value={this.state.itemConfig.token_num_uses}
+                            className={styles.textFieldStyle}
+                            hintText={`Secret ID TTL`}
+                            floatingLabelFixed={true}
+                            floatingLabelText='Secret ID TTL'
+                            fullWidth={false}
+                            autoFocus
+                            onChange={(e) => {
+                                this.setState({ itemConfig: update(this.state.itemConfig, { token_num_uses: { $set: e.target.value } }) });
+                            }}
+                        />,
+                        <TextField
+                            key='token_ttl'
+                            value={this.state.itemConfig.token_ttl}
+                            className={styles.textFieldStyle}
+                            hintText={`Token TTL`}
+                            floatingLabelFixed={true}
+                            floatingLabelText='Token TTL'
+                            fullWidth={false}
+                            autoFocus
+                            onChange={(e) => {
+                                this.setState({ itemConfig: update(this.state.itemConfig, { token_ttl: { $set: e.target.value } }) });
+                            }}
+                        />,
+                        <TextField
+                            key='token_max_ttl'
+                            value={this.state.itemConfig.token_max_ttl}
+                            className={styles.textFieldStyle}
+                            hintText={`Token Max TTL`}
+                            floatingLabelFixed={true}
+                            floatingLabelText='Token Max TTL'
+                            fullWidth={false}
+                            autoFocus
+                            onChange={(e) => {
+                                this.setState({ itemConfig: update(this.state.itemConfig, { token_max_ttl: { $set: e.target.value } }) });
+                            }}
+                        />,
+                        <TextField
+                            key='period'
+                            value={this.state.itemConfig.period}
+                            className={styles.textFieldStyle}
+                            hintText={`Period`}
+                            floatingLabelFixed={true}
+                            floatingLabelText='Period'
+                            fullWidth={false}
+                            autoFocus
+                            onChange={(e) => {
+                                this.setState({ itemConfig: update(this.state.itemConfig, { period: { $set: e.target.value } }) });
+                            }}
                         />
                     ]
                 )
@@ -214,9 +319,11 @@ export default class AppRoleAuthBackend extends React.Component {
             return (
                 <List>
                     {this.state.openNewItemDialog && renderNewFields()}
+                    {this.state.openEditItemDialog && renderEditFields()}
                     {renderConstantFields()}
                     <Subheader>Assigned Groups</Subheader>
                     <PolicyPicker
+                        key='policies'
                         type={`approle`}
                         item={`policies`}
                         height='250px'
@@ -231,26 +338,26 @@ export default class AppRoleAuthBackend extends React.Component {
         let renderNewDialog = () => {
 
             let validateAndSubmit = () => {
-                if (this.state.itemConfig.role_name === '') {
+                if (this.state.selectedItemName === '') {
                     snackBarMessage(new Error(`Name cannot be empty`));
                     return;
                 }
 
-                this.createUpdateItem(this.state.itemConfig.role_name);
+                this.createUpdateItem();
             }
 
             const actions = [
                 <FlatButton
                     label='Cancel'
                     onTouchTap={() => {
-                        this.setState({ openNewItemDialog: false, itemConfig: _.clone(this.oktaItemSchema) })
+                        this.setState({ openNewItemDialog: false, itemConfig: _.clone(this.itemConfigSchema), selectedItemName: '' });
+                        history.push(`${this.state.baseUrl}`);
                     }}
                 />,
                 <FlatButton
                     label='Create'
                     primary={true}
                     onTouchTap={validateAndSubmit}
-                    disableCreate={this.state.disableCreate}
                 />
             ];
 
@@ -269,7 +376,37 @@ export default class AppRoleAuthBackend extends React.Component {
         };
 
         let renderEditDialog = () => {
+            let validateAndSubmit = () => {
+                this.createUpdateItem();
+            }
 
+            const actions = [
+                <FlatButton
+                    label='Cancel'
+                    onTouchTap={() => {
+                        this.setState({ openEditItemDialog: false, itemConfig: _.clone(this.itemConfigSchema), selectedItemName: '' });
+                        history.push(`${this.state.baseUrl}`);
+                    }}
+                />,
+                <FlatButton
+                    label='Save'
+                    primary={true}
+                    onTouchTap={validateAndSubmit}
+                />
+            ];
+
+            return (
+                <Dialog
+                    title={`Editting ${this.state.selectedItemName}`}
+                    modal={false}
+                    actions={actions}
+                    open={this.state.openEditItemDialog}
+                    onRequestClose={() => this.setState({ openEditItemDialog: false, itemConfig: _.clone(this.itemConfigSchema) })}
+                    autoScrollBodyContent={true}
+                >
+                    {renderFields()}
+                </Dialog>
+            );
         };
 
         let renderListItems = () => {
@@ -293,7 +430,7 @@ export default class AppRoleAuthBackend extends React.Component {
                         leftAvatar={avatar}
                         rightIconButton={action}
                         onTouchTap={() => {
-                            this.setState({ itemConfig: _.clone(this.itemConfigSchema), selectedItemName: `${this.state.selectedTab}/${item}` });
+                            this.setState({ itemConfig: _.clone(this.itemConfigSchema), selectedItemName: `${item}` });
                             tokenHasCapabilities(['read'], `${this.state.baseVaultPath}/role/${item}`).then(() => {
                                 history.push(`${this.state.baseUrl}role/${item}`);
                             }).catch(() => {
@@ -310,7 +447,7 @@ export default class AppRoleAuthBackend extends React.Component {
         return (
             <div>
                 {this.state.openNewItemDialog && renderNewDialog()}
-                {this.state.openNewItemDialog && renderEditDialog()}
+                {this.state.openEditItemDialog && renderEditDialog()}
                 <VaultObjectDeleter
                     path={this.state.deleteUserPath}
                     onReceiveResponse={() => {
